5573ce6832ff3c19b64372ba32914bf1f94cb88a
Etni Alvis <etni.alvis@petalcard.com>
2022-03-09 11:22:58 -0500

    Initial

---
+++ ranks.py
@@ -0,0 +1,19 @@
+
+
+# 
+
+
+
+if __name__ == "__main__":
+    # 
+    urls = [] 
+
+    for url in urls: 
+        rankings = something(url)
+        # list of { rank , card name }
+
+        save into file 
+        compile csv file.
+        publish 
+
+
