e2b092836e6c49a2c07a7101889b968f60f86b7e
Etni Alvis <etni.alvis@petalcard.com>
2022-03-09 15:22:13 -0500

    cleanup rankings printing

--- ranks.py
+++ ranks.py
@@ -9,21 +9,29 @@ PETAL2 = r"Petal 2"
 
 
 def get_card_ranking(card, rankings) -> list:
-    return [item for item in rankings if re.search(card, item["card"])]
+    return [item['rank'] for item in rankings if re.search(card, item["card"])]
+
+def display_ranking(rankings, name) -> None:
+    print(f'List: {name} =================================== ')
+    for item in rankings:
+        print(f"{item['rank']} - {item['card']}")
 
 
 if __name__ == "__main__":
     # urls of web pages to scrape
     config = Config()
+    petal_rankings = []
     for item in config.list:
         web_scraper = scraper.determine_scraper(item["url"], item["name"])
         rankings: List[Dict] = web_scraper.get_rankings()
-        print (item["name"])
-        petal1_ranking = get_card_ranking(PETAL1, rankings)
-        petal2_ranking = get_card_ranking(PETAL2, rankings)
+        display_ranking(rankings, item["name"])
+        rank = { "name" : item["name"], 
+                 "petal1": get_card_ranking(PETAL1, rankings),
+                "petal2": get_card_ranking(PETAL2, rankings),
+        }
 
-        # save into file
-        # compile csv file.
-        # publish
-        pprint(petal1_ranking)
-        pprint(petal2_ranking)
+        petal_rankings.append(rank)
+ 
+
+    print("PETAL RANKINGS")
+    pprint(petal_rankings)
