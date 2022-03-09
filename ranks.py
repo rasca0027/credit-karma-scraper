b01268415047a10278de116d0ea105a02a1b2ab5
Etni Alvis <etni.alvis@petalcard.com>
2022-03-09 14:49:19 -0500

    integrate scraper with config

--- ranks.py
+++ ranks.py
@@ -1,16 +1,15 @@
 from typing import Dict, List
 import scraper
 from config import Config
+from pprint import pprint 
 
 if __name__ == "__main__":
     # urls of web pages to scrape
     config = Config()
-    
     for item in config.list:
-        # web_scraper = scraper.determine_scraper(url, page_title)
-        # rankings: List[Dict] = web_scraper.get_rankings()
-        print (item["name"])
-        # print(rankings)
+        web_scraper = scraper.determine_scraper(item["url"], item["name"])
+        rankings: List[Dict] = web_scraper.get_rankings()
+        pprint(rankings)
 
         # save into file
         # compile csv file.
