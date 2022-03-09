51d691903b76f7677e900373263a57f836985ff6
rasca0027 <kelly.chang@petalcard.com>
2022-03-09 14:13:13 -0500

    add in all web pages

--- ranks.py
+++ ranks.py
@@ -5,11 +5,22 @@ import scraper
 
 if __name__ == "__main__":
     # urls of web pages to scrape
-    urls = []
+    urls = [
+        ("https://www.creditkarma.com/credit-cards/student-cards", "Student Cards"),
+        ("https://www.creditkarma.com/credit-cards/no-annual-fee", "No Annual Fee"),
+        ("https://www.creditkarma.com/credit-cards/fair-credit", "Fair Credit"),
+        ("https://www.creditkarma.com/credit-cards/bad-credit", "Bad Credit"),
+        ("https://www.creditkarma.com/credit-cards/no-credit", "Limited Credit"),
+        ("https://www.creditkarma.com/credit-cards/good-credit", "Good Credit"),
+        ("https://www.creditkarma.com/credit-cards/cash-back-cards", "Cash Back Cards"),
+        ("https://www.creditkarma.com/credit-cards/rewards-cards", "Rewards"),
+    ]
 
-    for url in urls:
-        rankings: List[Dict] = scraper.determine_scraper(url)
-        # list of { rank , card name }
+    for url, page_title in urls:
+        web_scraper = scraper.determine_scraper(url, page_title)
+        rankings: List[Dict] = web_scraper.get_rankings()
+
+        print(rankings)
 
         # save into file
         # compile csv file.
--- scraper.py
+++ scraper.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
 from urllib import parse
+from typing import List, Dict
+import re
 
+from bs4 import BeautifulSoup
 import requests
 
 
@@ -17,21 +20,32 @@ class WebScraper(ABC):
 
 class CreditKarmaScraper(WebScraper):
     """WebScraper class for CreditKarma."""
-    def __init__(self, url):
+    def __init__(self, url, page_title):
         super().__init__(url)
         r = requests.get(url)
         self.html = r.text
+        self.page_title = page_title
 
-    def get_rankings(self):
-        pass
+    def get_rankings(self) -> List[Dict]:
+        soup = BeautifulSoup(self.html, 'html.parser')
+        all_cards = soup.find_all('h3')
+        rankings = []
+        for i, card in enumerate(all_cards):
+            cleaned_card_name = re.sub(r"[^a-zA-Z0-9 ]", "", card.text)
+            rankings.append({
+                "rank": i + 1,
+                "card": cleaned_card_name,
+                "source": self.page_title,
+            })
+        return rankings
 
 
-def determine_scraper(url) -> WebScraper:
+def determine_scraper(url: str, page_title: str) -> WebScraper:
     """Determines the website and which scraper to use."""
     domain = parse.urlparse(url).netloc
 
-    if domain == 'creditkarma.com':
-        return CreditKarmaScraper(url)
+    if domain == 'www.creditkarma.com':
+        return CreditKarmaScraper(url, page_title)
 
     # We only accepts certain websites now
     raise
