0940448d66066c0026a6586a5564699f1d477a98
rasca0027 <kelly.chang@petalcard.com>
2022-03-09 16:12:23 -0500

    fix styling

--- config.py
+++ config.py
@@ -1,27 +1,25 @@
+from typing import Dict, List
+
 import yaml
 
 DEFAULT_CONFIG = 'config.yml'
 
-class Config():
-    
-    def __init__(self, config_file: str = DEFAULT_CONFIG) -> None:
-        self._config = self.__read_config(config_file)
 
-    def __read_config(self, file_path) -> dict:    
+class Config():
+
+    def __init__(self, config_file: str = DEFAULT_CONFIG) -> None:
+        self._config = self._read_config(config_file)
+
+    def _read_config(self, file_path) -> Dict:
         with open(file_path, 'r') as f:
             return yaml.safe_load(f)
 
-
     @property
-    def list(self) -> list:
+    def list(self) -> List:
         return self._config['sources']
-
 
 
 if __name__ == '__main__':
     config = Config()
     for url in config.list:
-        print (url)
-
-
-
+        print(url)
--- ranks.py
+++ ranks.py
@@ -1,8 +1,8 @@
 from typing import Dict, List
 import scraper
 from config import Config
-from pprint import pprint 
-import re 
+from pprint import pprint
+import re
 
 PETAL1 = r"Petal 1"
 PETAL2 = r"Petal 2"
@@ -13,17 +13,20 @@ def get_card_ranking(card, rankings) -> list:
 
 
 if __name__ == "__main__":
-    # urls of web pages to scrape
+
+    # load urls of web pages to scrape
     config = Config()
-    for item in config.list:
-        web_scraper = scraper.determine_scraper(item["url"], item["name"])
+
+    for webpage in config.list:
+        web_scraper = scraper.determine_scraper(webpage["url"], webpage["name"])
         rankings: List[Dict] = web_scraper.get_rankings()
-        print (item["name"])
+
+        print(webpage["name"])
         petal1_ranking = get_card_ranking(PETAL1, rankings)
         petal2_ranking = get_card_ranking(PETAL2, rankings)
+        pprint(petal1_ranking)
+        pprint(petal2_ranking)
 
         # save into file
         # compile csv file.
         # publish
-        pprint(petal1_ranking)
-        pprint(petal2_ranking)
