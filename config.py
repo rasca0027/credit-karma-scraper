0f934a12d0acb6012a666f52a809aef61f1d311c
Etni Alvis <etni.alvis@petalcard.com>
2022-03-10 12:36:51 -0500

    Linting files

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-from typing import Dict, List
+"""Configuration for Card classic."""
+import datetime
 
 import yaml
-import datetime
 
 DEFAULT_CONFIG = "config.yml"
 SOURCES_NODE = "sources"
@@ -9,22 +9,30 @@ OUTPUTFILE_NODE = "outputfile"
 DATE_TAG = "{date}"
 DATE_FORMAT = "%Y-%m-%d"
 
+
 class Config:
+    """Configuration class?"""
+
     def __init__(self, config_file: str = DEFAULT_CONFIG) -> None:
         self._config = self._read_config(config_file)
 
-    def _read_config(self, file_path) -> dict:
-        with open(file_path, "r") as f:
-            return yaml.safe_load(f)
+    @staticmethod
+    def _read_config(file_path) -> dict:
+        """Loads the Configuration file."""
+        with open(file_path, "r") as file:
+            return yaml.safe_load(file)
 
     @property
     def list(self) -> list:
+        """Returns the sources as a list of dictionaries."""
         return self._config[SOURCES_NODE]
 
     @property
     def today(self) -> str:
+        """Returns today's date for use in the filename and on the csv file."""
         return datetime.datetime.now().strftime(DATE_FORMAT)
 
     @property
     def outputfile(self) -> str:
+        """Returns the path of the output file."""
         return self._config[OUTPUTFILE_NODE].replace(DATE_TAG, self.today)
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from typing import Dict, List, Union
-import scraper
-from config import Config
-import re
+"""Job for Card Rankings from CreditKarma."""
 import csv
 import os
+import re
+from typing import Dict
+from typing import List
+from typing import Union
+
+from config import Config
+import scraper
 
 
 class ExportCardRankingsJob:
-    """This job will scrap CreditKarma for the Daily Rankings
-    and create a csv file with the Petal1 and Petal2 ranking
+    """This job will scrap CreditKarma for the Daily Rankings.
+
+    Create a csv file with the Petal1 and Petal2 ranking
     on a number of lists that are configured on the config.yml file
     """
 
@@ -18,23 +23,27 @@ class ExportCardRankingsJob:
     def __init__(self, dry_run: bool = False) -> None:
         self.dry_run = dry_run
 
-    def _rank(self, card: str, rankings: List[dict]) -> Union[int, None]:
-        """returns the rank for a card in a ranking list"""
-        ranking = [item["rank"] for item in rankings if re.search(card, item["card"])]
+    @staticmethod
+    def _rank(card: str, rankings: List[dict]) -> Union[int, None]:
+        """Returns the rank for a card in a ranking list."""
+        ranking = [
+            item["rank"] for item in rankings if re.search(card, item["card"])
+        ]
         return ranking[0] if ranking else None
 
-    def _get_card_rankings(self, name: str, url: str) -> List[Dict]:
-        """calls the scraper that reads the url and returns the rankings"""
+    @staticmethod
+    def _get_card_rankings(name: str, url: str) -> List[Dict]:
+        """Calls the scraper that reads the url and returns the rankings."""
         print("Reading Rankings From", url)
         web_scraper = scraper.determine_scraper(url, name)
         return web_scraper.get_rankings()
 
     def _save(self, data: List[dict], filename: str) -> None:
-        """converts the list of petal rankings into a csv file and saves into a file"""
+        """Converts the list of petal rankings into a csv file and saves into a file."""
         self._ensure_dir_exists(filename)
         header = list(data[0].keys())
-        with open(filename, "w", encoding="UTF8") as f:
-            writer = csv.DictWriter(f, fieldnames=header)
+        with open(filename, "w", encoding="UTF8") as file:
+            writer = csv.DictWriter(file, fieldnames=header)
             writer.writeheader()
             writer.writerows(data)
 
@@ -44,28 +53,32 @@ class ExportCardRankingsJob:
 
     @staticmethod
     def _display_ranking(rankings: list, name: str) -> None:
-        """helper method to display the rankings for a list"""
+        """Helper method to display the rankings for a list."""
         print(f"List: {name} =================================== ")
         for item in rankings:
             print(f"{item['rank']} - {item['card']}")
 
     def run(self) -> None:
+        """Entry point for the job."""
         config = Config()
         today = config.today
 
         petal_rankings = []
-        
+
         for item in config.list:
             rankings = self._get_card_rankings(item["name"], item["url"])
             self._display_ranking(rankings, item["name"])
-            petal_rankings.append({
-                "date": today,
-                "page_title": item["name"],
-                "p1_rank": self._rank(self.PETAL1, rankings) or "N/A",
-                "p2_rank": self._rank(self.PETAL2, rankings) or "N/A",
-            })
+            petal_rankings.append(
+                {
+                    "date": today,
+                    "page_title": item["name"],
+                    "p1_rank": self._rank(self.PETAL1, rankings) or "N/A",
+                    "p2_rank": self._rank(self.PETAL2, rankings) or "N/A",
+                }
+            )
 
         self._save(petal_rankings, config.outputfile)
+
 
 if __name__ == "__main__":
     app = ExportCardRankingsJob()
